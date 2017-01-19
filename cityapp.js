d34844b77c82e5e01527e876b5a6efd314722440
nmrosenthal <nmrosenthal@gmail.com>
2017-01-19 10:26:59 +0200

    python

--- cityapp.js
+++ cityapp.js
@@ -7,14 +7,9 @@
     function initMap() {
         var uluru = {lat: 39.1609, lng: -77.2206};
         mymap = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
+          zoom: 9,
           center: uluru
         });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: mymap
-        });
-        console.log(mymap);
     	$.ajax({
 		    url: "https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?color=RED&$where=description like '%25EXCEED%25' AND latitude > 39.02 AND longitude < 0 AND date_of_stop between '2015-01-10T00:00:00' and '2016-01-10T00:00:00'",
 		    type: "GET",
@@ -23,18 +18,14 @@
 		      "$$app_token" : "EomQIfjQBBVCOkhua3dU0818w"
     		}
 		}).done(function(data){
-			$("#notification").text("Retrieved " + data.length + " records from the dataset!");
+			$("#notification").text("There were " + data.length + " speeding violations in the selected time period.");
 			for (i in data) {
 				// console.log(data[i].description);
 				// console.log(data[i].date_of_stop);
 				// console.log(data[i].time_of_stop);
 			  	// console.log(data[i].color + " " + data[i].race + " " + data[i].latitude + " " + data[i].longitude);
 			  	var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
-			  	ll.push(latLng);
-        		var marker = new google.maps.Marker({
-            	position: latLng,
-            	map: mymap
-          });
+			  	ll.push(latLng);        		
 	  	}
 	  	 heatmap = new google.maps.visualization.HeatmapLayer({
           data: ll,
@@ -46,4 +37,4 @@
       });
 }
 
-	console.log(mymap.getMap());
+	console.log(mymap);
