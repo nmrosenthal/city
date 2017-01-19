2b8603ad27f5eeb150365aec59605cc26d94a044
nmrosenthal <nmrosenthal@gmail.com>
2017-01-19 15:51:41 +0200

    demo version

--- cityapp.js
+++ cityapp.js
@@ -4,30 +4,28 @@
 	function initMap() {
 	    var montg = {lat: 39.1609, lng: -77.2206};
 	    mymap = new google.maps.Map(document.getElementById('map'), {
-	        zoom: 9,
-	        center: montg
+	    zoom: 9,
+	    center: montg
 	    });
-	}
+	} 
 
-	function displayData(firstD,secondD, firstT,secondT) {
-		console.log("https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?color=RED&$where=description like '%25EXCEED%25' AND latitude > 39.02 AND longitude < 0 AND date_of_stop between '"+firstD+"T00:00:00' and '"+ secondD + "T00:00:00' AND time_of_stop between '" + firstT + "' and '" + secondT +"'");
+	function displayData(firstD,secondD) {
 		  	$.ajax({
-		    url: "https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?color=RED&$where=description like '%25EXCEED%25' AND latitude > 39.02 AND longitude < 0 AND date_of_stop between '"+firstD+"T00:00:00' and '"+ secondD + "T00:00:00'", 
+		    url: "https://data.montgomerycountymd.gov/resource/ms8i-8ux3.json?$where=description like '%25EXCEED%25' AND date_of_stop between '"+firstD+"T00:00:00' and '"+ secondD + "T00:00:00'", 
 		    type: "GET",
 		    data: {
 		      "$limit" : 10000,
 		      "$$app_token" : "EomQIfjQBBVCOkhua3dU0818w"
     		}
 		}).done(function(data){
+			console.log(data);
 			var ll = [];
 			$("#notification").text("Retrieved " + data.length + " Records From The Data Set!");
 			for (i in data) {
-				// console.log(data[i].description);
-				// console.log(data[i].date_of_stop);
-				console.log(data);
-			 //  	console.log(data[i].color + " " + data[i].race + " " + data[i].latitude + " " + data[i].longitude);
+				if(isNaN(data[i].latitude)===false && isNaN(data[i].longitude)===false) {
 			  	var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
 			  	ll.push(latLng);        		
+			  }
 	  		}
 	  	 	heatmap = new google.maps.visualization.HeatmapLayer({
           		data: ll,
@@ -36,15 +34,13 @@
       	});
 	}
 
-	function toggleHeatmap() {
-        	mymap.setMap(heatmap.getMap() ? null : heatmap);
-      		};
 
-
-    $("#submitForm").click(function() {
+    $("#submitForm").click(function(e) {
+    	e.preventDefault();
+    	e.stopPropagation();
     	var firstDate = ($("#firstDate").val());
     	var secondDate = ($("#secondDate").val());
     	var firstTime = ($("#firstTime").val()+":00");
     	var secondTime= ($("#secondTime").val()+":00");
-    	displayData(firstDate,secondDate,firstTime,secondTime);
+    	displayData(firstDate,secondDate);
     });
